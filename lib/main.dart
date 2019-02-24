ac50f79fe38ce8ef09a7edae2bf8c215ccd49909
Benjamin Soung <soungbenjamin@gmail.com>
2019-02-24 02:02:04 -0500

    adding floating button

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
-      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Hi there!');
+        },
+        child: Text('+'),
+      ),
+      appBar: AppBar(
+        title: Text('Images'),
+      ),
     ),
   );
 
