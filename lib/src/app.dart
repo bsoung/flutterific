cf1a0846f25ba3886ee2094b5018a6d7db8baf58
Benjamin Soung <soungbenjamin@gmail.com>
2019-02-24 03:56:57 -0500

    add async get

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
+import './models/image_model.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
   createState() => AppState();
@@ -8,8 +10,10 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
-  void fetchImage() {
-
+  void fetchImage() async {
+    counter += 1;
+    final response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
   }
 
   Widget build(context) {
