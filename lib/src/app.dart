5e33df010cf39c025d65b2d1ece8550a521555f7
Benjamin Soung <soungbenjamin@gmail.com>
2019-02-24 03:41:28 -0500

    add http get

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   createState() => AppState();
@@ -7,16 +8,16 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
+  void fetchImage() {
+
+  }
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
           child: Icon(Icons.add),
         ),
         appBar: AppBar(
