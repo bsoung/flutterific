1fdf0a74c9357a6f4fd9f70bfd4a94a631964495
Benjamin Soung <soungbenjamin@gmail.com>
2019-02-24 04:15:53 -0500

    add widget folder and list widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import './models/image_model.dart';
+import './widgets/image_list.dart';
 import 'dart:convert';
 
 class App extends StatefulWidget {
@@ -9,17 +10,23 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     counter += 1;
+
     final response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
-    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           onPressed: fetchImage,
           child: Icon(Icons.add),
